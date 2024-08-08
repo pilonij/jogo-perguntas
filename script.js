85ba69ea1941ba8a54b6e2eb4cc8276dbca01ec4
julia <108429786+pilonij@users.noreply.github.com>
2024-08-08 08:02:33 -0300

    efegedg

---
+++ script.js
@@ -0,0 +1,21 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('.texto-resultado');
+
+const perguntas = [
+                {
+                    enunciado : "No âmbito social, você prefere:",
+                    alternativas: ["Alternaaativa 1", "Alternativa 2"]
+                },
+                {
+                    enunciado : "No âmbito ambiental, você prefere:",
+                    alternativas: ["Alternaaativa 3", "Alternativa 4"]
+                },
+                {
+                    enunciado : "No âmbito tecnológico, você prefere:",
+                    alternativas: ["Alternaaativa 5", "Alternativa 6"]
+                },
+
+]
