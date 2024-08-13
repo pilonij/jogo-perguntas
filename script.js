6a345abda310bfa037b6dbb5fbe22148fb04fce1
julia <108429786+pilonij@users.noreply.github.com>
2024-08-13 08:09:37 -0300

    .

--- script.js
+++ script.js
@@ -5,27 +5,63 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
-                {
-                    enunciado : "Você vê uma pessoa que possui deficiência com dificuldades para se alimentar. ",
-                    alternativas: [" Você ajuda ela a se alimentar, mas ela diz que você deveria virar deficiência para comer menos também ", ". Você ri e segue sua vida, mas é atropelado em seguida e vira deficiente também."]
-                },
-                {
-                    enunciado : " Você trabalha em uma grande produtora de papel e descobre que eles estão desmatando árvores de forma descontrolada e conta a lei ",
-                    alternativas: ["Você garante seu emprego e não fala nada, é uma árvore da espécie da espécie Pinheiro entra em extinção", " Você denúncia as autoridades ambientais locais para que providências sejam tomadas, mas perde seu emprego"]
-                },
-                {
-                    enunciado : "Uma empresa grande de produção de filmes quer te contratar para elaborar dublagens com IA.",
-                    alternativas: [" Voce faz o que a empresa pede,e destrói o trabalho dos outros, mas ganha um aumento", "Você desenvolve uma campanha de incentivo a dublagem viva, mas outra pessoa é contratada e você fica desempregado"]
-                },
+    {
+        enunciado: "Você vê uma pessoa que possui deficiência com dificuldades para se alimentar. ",
+        alternativas: [
+            {
+                texto: " Você ajuda ela a se alimentar, mas ela diz que você deveria virar deficiência para comer menos também ",
+                afirmacao: "afirmacao",
+            },
+            {
+                texto: ". Você ri e segue sua vida, mas é atropelado em seguida e vira deficiente também.",
+                afirmacao: "afirmacao",
+            },]
+    },
+    {
+        enunciado: " Você trabalha em uma grande produtora de papel e descobre que eles estão desmatando árvores de forma descontrolada e conta a lei. ",
+        alternativas:[
+            {
+                texto: " Você garante seu emprego e não fala nada, é uma árvore da espécie da espécie Pinheiro entra em extinção. ",
+                afirmacao: "afirmacao,"
+            },
+            {
+                texto: "Você denúncia as autoridades ambientais locais para que providências sejam tomadas, mas perde seu emprego.",
+                afirmacao: "afirmacao",
+            },
+                        ]
+},
+{
+    enunciado: "Uma empresa grande de produção de filmes quer te contratar para elaborar dublagens com IA.",
+    alternativas: [  
+        {
+            texto: "Voce faz o que a empresa pede,e destrói o trabalho dos outros, mas ganha um aumento.",
+            afirmacao: "afirmacao",
+        },
+        {
+            texto:"Você desenvolve uma campanha de incentivo a dublagem viva, mas outra pessoa é contratada e você fica desempregado. ",
+            afirmacao: "afirmacao",
+        },
+            ]
+},
 
 ]
 
 let atual = 0;
 let perguntaAtual;
 
-function mostraPerguntas(){
+function mostraPerguntas() {
     perguntaAtual = perguntas[atual];
-    caixaPerguntas . textContent = perguntaAtual.enunciado;
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+
 
 mostraPerguntas();
