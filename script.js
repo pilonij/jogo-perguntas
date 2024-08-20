d3855d17e77a134626bcd449bb9fae7782862a94
julia <108429786+pilonij@users.noreply.github.com>
2024-08-20 08:40:58 -0300

    nb

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const perguntas = [
     },
     {
         enunciado: " Você trabalha em uma grande produtora de papel e descobre que eles estão desmatando árvores de forma descontrolada e conta a lei. ",
-        alternativas:[
+        alternativas: [
             {
                 texto: " Você garante seu emprego e não fala nada, é uma árvore da espécie da espécie Pinheiro entra em extinção. ",
                 afirmacao: "afirmacao,"
@@ -28,55 +28,55 @@ const perguntas = [
                 texto: "Você denúncia as autoridades ambientais locais para que providências sejam tomadas, mas perde seu emprego.",
                 afirmacao: "afirmacao",
             },
-                        ]
-},
-{
-    enunciado: "Uma empresa grande de produção de filmes quer te contratar para elaborar dublagens com IA.",
-    alternativas: [  
-        {
-            texto: "Voce faz o que a empresa pede,e destrói o trabalho dos outros, mas ganha um aumento.",
-            afirmacao: "afirmacao",
-        },
-        {
-            texto:"Você desenvolve uma campanha de incentivo a dublagem viva, mas outra pessoa é contratada e você fica desempregado. ",
-            afirmacao: "afirmacao",
-        },
-            ]
-},
+        ]
+    },
+    {
+        enunciado: "Uma empresa grande de produção de filmes quer te contratar para elaborar dublagens com IA.",
+        alternativas: [
+            {
+                texto: "Voce faz o que a empresa pede,e destrói o trabalho dos outros, mas ganha um aumento.",
+                afirmacao: "afirmacao",
+            },
+            {
+                texto: "Você desenvolve uma campanha de incentivo a dublagem viva, mas outra pessoa é contratada e você fica desempregado. ",
+                afirmacao: "afirmacao",
+            },
+        ]
+    },
 
 ]
 
 let atual = 0;
 let perguntaAtual;
-let historiaFinal="";
+let historiaFinal = "";
 
 function mostraPerguntas() {
-    if(atual >= perguntas.length){
+    if (atual >= perguntas.length) {
         mostraResultado();
         return;
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent ="";
-    mostraAlternativas ();
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
-function mostraAlternativas(){
+function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-function respostaSelecionada (alternativa) {
-  const afirmacoes = alternativa.afirmacao;
-  historiaFinal += afirmacoes + "";
-     atual++;
-     mostraPergunta();
+function respostaSelecionada(alternativa) {
+    const afirmacoes = alternativa.afirmacao;
+    historiaFinal += afirmacoes + "";
+    atual++;
+    mostraPerguntas();
 }
 
-function mostraResultado(){
+function mostraResultado() {
     caixaPerguntas.textContent = "Em resumo, voce escolheu..."
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
